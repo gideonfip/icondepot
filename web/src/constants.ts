97a0057f1a0807aae968ff54d0c7b8b018655c18
fipcryptoX <fipharmacistsg@gmail.com>
2026-03-31 21:05:49 +0800

    feat(icons): add metamask, okx, taiko, avalanche, and eigenlayer sets

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,8 +1,8 @@
-export const BASE_URL = "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons"
-export const REPO_PATH = "https://github.com/homarr-labs/dashboard-icons"
-export const METADATA_URL = "https://raw.githubusercontent.com/homarr-labs/dashboard-icons/refs/heads/main/metadata.json"
+export const BASE_URL = "https://cdn.jsdelivr.net/gh/gideonfip/dashboard-icons"
+export const REPO_PATH = "https://github.com/gideonfip/dashboard-icons"
+export const METADATA_URL = "https://raw.githubusercontent.com/gideonfip/dashboard-icons/main/metadata.json"
 export const WEB_URL = "https://dashboardicons.com"
-export const REPO_NAME = "homarr-labs/dashboard-icons"
+export const REPO_NAME = "gideonfip/dashboard-icons"
 
 export const getDescription = (totalIcons: number) =>
 	`A collection of ${totalIcons} curated icons for services, applications and tools, designed specifically for dashboards and app directories.`
